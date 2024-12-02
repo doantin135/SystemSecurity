aab377564f208ccca5d07474da38ff1fea141030
NTBDuy <duyntb2001@gmail.com>
2024-12-02 17:08:37 +0700

    fix name

--- project/final/src/App.js
+++ project/final/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
-import Login from './components/LoginCo';
-import Register from './components/RegisterCo';
+import Login from './components/Login';
+import Register from './components/Register';
 import Home from './components/Home';
 
 function App() {
